29292f5e52ea9ca6c4096357761dac6b1622fae3
pranavvdesai <pranavdesai.psd@gmail.com>
2024-03-30 16:16:17 +0530

    fix: contrCT

--- packages/hardhat/contracts/HackathonManager.sol
+++ packages/hardhat/contracts/HackathonManager.sol
@@ -10,8 +10,12 @@ contract HackathonManager is ERC20 {
     // Struct for hackathons
     struct Hackathon {
         string name;
+        string organisedby;
         string description;
         uint256 date;
+        string city;
+        string exp;
+        string category;
         address[] hackers;
         Sponsor[] sponsors;
     }
@@ -23,7 +27,6 @@ contract HackathonManager is ERC20 {
         uint256 threshold;
         uint256 lockedAmount;
         uint256 tokenAmount;
-        bool isGold;
     }
 
     mapping(uint256 => Hackathon) public hackathons;
@@ -35,12 +38,16 @@ contract HackathonManager is ERC20 {
     }
 
     // Function to create a new hackathon
-    function createHackathon(string memory _name, string memory _description, uint256 _date) public payable {
+    function createHackathon(string memory _name, string memory _organisedby, string memory _description, uint256 _date, string memory _city, string memory _exp, string memory _category) public payable {
         require(msg.value == 4 wei, "Stake amount should be 4 ETH");
         Hackathon storage newHackathon = hackathons[hackathonCount];
         newHackathon.name = _name;
+        newHackathon.organisedby = _organisedby;
         newHackathon.description = _description;
         newHackathon.date = _date;
+        newHackathon.city = _city;
+        newHackathon.exp = _exp;
+        newHackathon.category = _category;
         newHackathon.hackers.push(msg.sender);
         xhacksToken.transfer(msg.sender, 4 * 10**18);
         hackathonCount++;
@@ -55,15 +62,14 @@ contract HackathonManager is ERC20 {
     }
 
     // Function for sponsors to pledge Ether
-    function sponsorHackathon(uint256 _hackathonId, string memory _name, uint256 _threshold, bool _isGold) public payable {
+    function sponsorHackathon(uint256 _hackathonId, string memory _name, uint256 _threshold) public payable {
         Hackathon storage hackathon = hackathons[_hackathonId];
         Sponsor memory newSponsor = Sponsor({
             sponsorAddress: payable(msg.sender),
             name: _name,
             threshold: _threshold,
             lockedAmount: msg.value,
-            tokenAmount: msg.value,
-            isGold: _isGold
+            tokenAmount: msg.value
         });
         hackathon.sponsors.push(newSponsor);
     }
@@ -81,18 +87,32 @@ contract HackathonManager is ERC20 {
     }
 
     // Getter functions
-    function getHackathonDetails(uint256 _hackathonId) public view returns (string memory, string memory, uint256, uint256) {
+    function getHackathonDetails(uint256 _hackathonId) public view returns (string memory, string memory, string memory, uint256, string memory, string memory, string memory, uint256) {
         Hackathon storage hackathon = hackathons[_hackathonId];
-        return (hackathon.name, hackathon.description, hackathon.date, hackathon.hackers.length);
+        return (hackathon.name,hackathon.organisedby, hackathon.description, hackathon.date, hackathon.city, hackathon.exp, hackathon.category, hackathon.hackers.length);
     }
 
     function getHackerTokenBalance(address _hacker) public view returns (uint256) {
         return xhacksToken.balanceOf(_hacker);
     }
 
-    function getSponsorDetails(uint256 _hackathonId, uint256 _sponsorIndex) public view returns (string memory, uint256, uint256, uint256, bool) {
+    function getSponsorDetails(uint256 _hackathonId, uint256 _sponsorIndex) public view returns (string memory, uint256, uint256, uint256) {
         Hackathon storage hackathon = hackathons[_hackathonId];
         Sponsor storage sponsor = hackathon.sponsors[_sponsorIndex];
-        return (sponsor.name, sponsor.threshold, sponsor.lockedAmount, sponsor.tokenAmount, sponsor.isGold);
+        return (sponsor.name, sponsor.threshold, sponsor.lockedAmount, sponsor.tokenAmount);
+    }
+
+    function transferTokensToContract(uint256 _amount) public {
+        require(_amount > 0, "Transfer amount must be greater than zero");
+        require(xhacksToken.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
+        xhacksToken.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function getAllHackathons() public view returns (Hackathon[] memory) {
+        Hackathon[] memory allHackathons = new Hackathon[](hackathonCount);
+        for (uint256 i = 0; i < hackathonCount; i++) {
+            allHackathons[i] = hackathons[i];
+        }
+    return allHackathons;
     }
 }
