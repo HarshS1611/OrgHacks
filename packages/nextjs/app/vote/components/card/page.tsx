c9f221de3ca091ff374759b9bab486a89326a973
pranavvdesai <pranavdesai.psd@gmail.com>
2024-03-31 00:31:23 +0530

    feat: voting

--- packages/nextjs/app/vote/components/card/page.tsx
+++ packages/nextjs/app/vote/components/card/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 // components/Card.js
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
+import HackathonManager from "../../../../../hardhat/artifacts/contracts/HackathonManager.sol/HackathonManager.json";
+import { contract_add } from "../../../../../hardhat/config";
+import { ethers } from "ethers";
+import Web3 from "web3";
+import Web3Modal from "web3modal";
 
 interface CardProps {
   title: string;
@@ -9,11 +16,35 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ title, map, onClick }) => {
+  const [voteCount, setVoteCount] = useState(0);
+  const handleVoteChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Parse the input value as a number and update the voteCount state
+    const newVoteCount = parseInt(event.target.value, 10);
+    setVoteCount(newVoteCount);
+  };
+  const vote = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const web3 = new Web3(connection);
+    const accounts = await web3.eth.getAccounts();
+    const account = accounts[0]; // Assuming you want the first account
+
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const jobPortal = new ethers.Contract(contract_add, HackathonManager.abi, signer);
+
+    const balance = await jobPortal.balanceOf(account);
+
+    const approval = await jobPortal.approve("0x5ED7019fCA0E2eD33167c2792c07d1751a326873", balance);
+    const tx = await jobPortal.transferTokensToContract(ethers.utils.parseEther(voteCount.toString()));
+    await tx.wait();
+    console.log(tx);
+
+    onClick();
+  };
+
   return (
-    <div
-      className="relative cursor-pointer mx-6 px-2 py-4 bg-[#242731] h-max w-64 sm:w-auto rounded-[20px] shadow-xl"
-      onClick={onClick}
-    >
+    <div className="relative cursor-pointer mx-6 px-2 py-4 bg-[#242731] h-max w-64 sm:w-auto rounded-[20px] shadow-xl">
       <div className="flex flex-col">
         <div className="flex items-center justify-start mx-5 mt-1.5 gap-x-5">
           <div className="flex flex-col">
@@ -41,17 +72,18 @@ const Card: React.FC<CardProps> = ({ title, map, onClick }) => {
           Organized by:
           <span className="text-blueLight font-medium">Devfolio</span>
         </p>
+        <div className="flex items-center mx-6">
+          <input
+            type="text"
+            className="flex-grow p-2 rounded-sm bg-white"
+            placeholder="Vote your xHack tokens"
+            onChange={handleVoteChange}
+          ></input>
+          <button className="ml-2 px-4 py-2 rounded-sm bg-blue-500 text-white" onClick={vote}>
+            Vote
+          </button>
+        </div>
       </div>
-      <iframe
-      className="px-4 py-10 rounded-lg"
-        src={map}
-        width="800"
-        height="400"
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
     </div>
   );
 };
