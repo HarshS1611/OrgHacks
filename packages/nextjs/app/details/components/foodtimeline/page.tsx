ae4139b53d0308e1cac0bfb2d2f82da246caf275
Sahas <sahasvivek001@gmail.com>
2024-03-30 11:04:11 +0530

    feat: component changes

--- packages/nextjs/app/details/components/foodtimeline/page.tsx
+++ packages/nextjs/app/details/components/foodtimeline/page.tsx
@@ -12,45 +12,50 @@ const FoodTimeline = () => {
   return (
     <ol className="relative border-s-2 border-gray-200 dark:border-blue-500  border-dashed">
       <li className="mb-10 ms-6">
-        <span className="absolute bg-green-300 flex items-center justify-center w-10 h-10 rounded-full -start-5   ">
+        <span className="absolute bg-green-300 flex items-center justify-center w-10 h-10 rounded-full -start-5">
           <p className="bg-green-300 text-white rounded-full px-3 py-1">1</p>
         </span>
-        <details className="ml-4 px-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
-          <div className="mb-4">venue</div>
+        <details className="ml-4 p-3 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
           <summary className="flex justify-between">
-            <p>venue description</p>
+            <div className="mb-4">Best Caterer</div>
             <div className="mx-2 flex w-10 items-center justify-center">
               <IoIosArrowDown />
             </div>
           </summary>
+          <p>Big Caterer will be one with best quality food with state of the art facilities, modern cuisines, etc..</p>
+          <p>5 star</p>
         </details>
       </li>
       <li className="mb-10 ms-6">
         <span className="absolute bg-green-300 flex items-center justify-center w-10 h-10 rounded-full -start-5   ">
           <p className="bg-green-300 text-white rounded-full px-3 py-1">2</p>
         </span>
-        <details className="px-4 ml-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
-          <div className="mb-4">venue</div>
+        <details className="ml-4 p-3 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
           <summary className="flex justify-between">
-            <p>venue description</p>
+            <div className="mb-4">Average Caterer</div>
             <div className="mx-2 flex w-10 items-center justify-center">
               <IoIosArrowDown />
             </div>
           </summary>
+          <p>
+            Average Caterer will be one with decent quality food with above average standards, multi cuisines, etc..
+          </p>
+          <p>3.5 star</p>
         </details>
       </li>
       <li className="ms-6">
         <span className="absolute bg-green-300 flex items-center justify-center w-10 h-10 rounded-full -start-5   ">
-          <p className="bg-green-300 text-white rounded-full px-3 py-1">3</p>
+          <p className="bg-green-300 text-white rounded-full px-3 py-1">1</p>
         </span>
-        <details className="px-4 ml-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
-          <div className="mb-4">venue</div>
+        <details className="ml-4 p-3 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
           <summary className="flex justify-between">
-            <p>venue description</p>
+            <div className="mb-4">Mediocre Caterer</div>
             <div className="mx-2 flex w-10 items-center justify-center">
               <IoIosArrowDown />
             </div>
           </summary>
+          <p>Small Caterer will be one with mediocre quality food with average standards, limited cuisines, etc..</p>
+          <p>2 star</p>
         </details>
       </li>
     </ol>
--- packages/nextjs/app/vote/page.tsx
+++ packages/nextjs/app/vote/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import Question from "./components/questions/page";
+import GoBackbtn from "~~/components/GoBack";
 
 const Home = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
@@ -36,7 +37,31 @@ const Home = () => {
     return <div className="text-center mt-10 text-2xl font-bold">You have completed the questions.</div>;
   }
 
-  return <Question options={questions[currentQuestion].options} onOptionSelect={handleOptionSelect} />;
+  return (
+    <>
+      <GoBackbtn />
+      <div className="bg-[#1a1e27] rounded-xl p-5 mt-7 mx-6">
+        <div className="flex flex-col md:flex-row my-2">
+          <h3 className="text-lg font-semibold md:ml-2">Name:</h3>
+
+          <p className="text-sm md:ml-2 mt-1">Big Hall</p>
+        </div>
+        <div className="flex flex-col md:flex-row my-2">
+          <h3 className="text-lg font-semibold md:ml-2">Description:</h3>
+
+          <p className="text-sm md:ml-2 mt-1">
+            Big Hall will be situated in the center of the city with all state of the art facilities
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row mx-2 my-2">
+          <h3 className="text-lg font-semibold">Expected accommodation:</h3>
+
+          <p className="text-sm md:ml-2 mt-1">2k</p>
+        </div>
+      </div>
+      <Question options={questions[currentQuestion].options} onOptionSelect={handleOptionSelect} />
+    </>
+  );
 };
 
 export default Home;
