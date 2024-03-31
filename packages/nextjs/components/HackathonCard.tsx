f0255d8710fa867e4e50c30942a833eea02e88d3
Sahas <sahasvivek001@gmail.com>
2024-03-31 05:55:38 +0530

    fix: ux

--- packages/nextjs/components/HackathonCard.tsx
+++ packages/nextjs/components/HackathonCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FC } from "react";
+import { useState } from "react";
 import Link from "next/link";
 
 // import Image from "next/image";
@@ -8,6 +9,10 @@ import Link from "next/link";
 
 // const ModelCard: FC<{ model: any }> = ({ model }) => {
 const HackathonCard = ({ index, props }: any) => {
+  const [status, setStatus] = useState("registration");
+
+  console.log(props);
+
   return (
     <div className="relative bg-[#242731] w-64 sm:w-auto rounded-[20px] shadow-xl">
       <div className="flex flex-col">
@@ -18,19 +23,21 @@ const HackathonCard = ({ index, props }: any) => {
               <span className={`text-xs font-medium text-white bg-gray-700 px-3 py-1 rounded-lg`}>
                 {props.category}
               </span>
-              <span className={`text-xs font-medium text-white bg-gray-700 px-3 py-1 rounded-lg`}>
-                <div className="flex gap-x-2 items-center">
-                  {/* <Image src="/assets/demo-icon.svg" width={15} height={15} alt="demo" /> */}
-                  <span>Registration open</span>
-                </div>
-              </span>
+              {/* <Image src="/assets/demo-icon.svg" width={15} height={15} alt="demo" /> */}
+              {status === "registration" ? (
+                <span className="bg-[#25a338] text-xs font-medium text-white px-3 py-1 rounded-lg">Registration</span>
+              ) : status === "voting" ? (
+                <span className=" bg-[#25306e] text-xs font-medium text-white px-3 py-1 rounded-lg">Ongoing</span>
+              ) : (
+                <span className="bg-[#5f1e29] text-xs font-medium text-white px-3 py-1 rounded-lg">Ended</span>
+              )}
             </div>
           </div>
         </div>
         <hr className="border-[#2B2F3D] mx-5" />
         <p className="text-[#B2B4C6] text-sm px-5">{props.description}</p>
         <div className="flex items-center gap-x-3.5 mx-5">
-          <p className="flex gap-x-1 text-[#B2B4C6] text-xs">Voters: 3.2k</p>
+          <p className="flex gap-x-1 text-[#B2B4C6] text-xs">Voters: {props[7].length}</p>
         </div>
         <p className="flex gap-x-1 text-[#B2B4C6] text-xs mx-5 my-1.5">
           Organized by:
@@ -38,7 +45,9 @@ const HackathonCard = ({ index, props }: any) => {
         </p>
       </div>
       <Link
-        href={`/details/${index}`}
+        href={
+          status === "registration" ? `/details/${index}` : status === "voting" ? `/vote/${index}` : `/details/${index}`
+        }
         //   onClick={() => {
         //     router.push(`/models/${model._id}`);
         //   }}
